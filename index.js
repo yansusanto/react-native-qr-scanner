ed55300d62cdf9be09ad286779482c595e054c97
Yan Susanto <yansusanto@gmail.com>
2023-03-12 22:58:13 +0800

    first edit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by marno on 2017/4/10
+ * Function: 入口类
+ * Desc:
+ */
+
+import QRScannerView, { QRScannerRectView } from './QRScanner';
+
+export { QRScannerView, QRScannerRectView }
